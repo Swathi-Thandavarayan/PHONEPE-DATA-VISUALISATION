b50a06ca600aca4387cf48ece50990b151a68472
Swathi-Thandavarayan <159911444+Swathi-Thandavarayan@users.noreply.github.com>
2024-03-27 19:20:38 +0530

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import mysql.connector
+import matplotlib.pyplot as plt
+import plotly
+
+config = {
+    "user": "root",
+    "password": "1234",
+    "host": "127.0.0.1",
+    "database": "phonepe_pulse"
+}
+connection = mysql.connector.connect(**config)
+cursor = connection.cursor()
+
+#STREAMLIT
+st.title(" PHONEPE PULSE DATA VISUALISATION  AND EXPLORATION ")
+st.subheader("A  User-Friendly Tool Using Streamlit and Plotly")
