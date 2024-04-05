13d1fe410bf300317733d84dd02d6b49c3c50589
Swathi-Thandavarayan <159911444+Swathi-Thandavarayan@users.noreply.github.com>
2024-04-05 17:21:13 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,9 @@ import streamlit as st
 import mysql.connector
 import matplotlib.pyplot as plt
 import plotly
+import pydeck as pdk
+import plotly.express as px
+import pandas as pd
 
 config = {
     "user": "root",
@@ -13,7 +16,44 @@ connection = mysql.connector.connect(**config)
 cursor = connection.cursor()
 
 #STREAMLIT
-st.title(" PHONEPE PULSE DATA VISUALISATION  AND EXPLORATION ")
-st.subheader("A  User-Friendly Tool Using Streamlit and Plotly")
-st.write("welcome")
-st.button("transform")
+
+st.set_page_config(
+    page_title="PHONEPE DATA VISUALISATION",
+    layout="centered" # wide or centered
+    )
+with st.sidebar:
+    st.title(" PHONEPE PULSE DATA VISUALISATION  AND EXPLORATION ")
+    st.subheader("A  User-Friendly Tool Using Streamlit and Plotly")
+
+st.title
+
+clm1,clm2,clm3 = st.columns(3)
+with clm1:
+    aggtrans = '''SELECT * FROM  top_users_district'''
+    cursor.execute(aggtrans)
+    df = pd.DataFrame()
+    results = cursor.fetchall()
+    df = pd.DataFrame(results)
+    
+    
+
+    if st.button("AGGREGATED"):
+       st.write("hello")
+        
+     
+        
+        
+with clm2:
+    st.header("MAPS")
+    aggtrans = '''SELECT * FROM  top_users_district'''
+    cursor.execute(aggtrans)
+    df = pd.DataFrame()
+    results = cursor.fetchall()
+    df = pd.DataFrame(results)
+    #query = '''SELECT COLUMN_NAME FROM FROM top_users_district'''
+    #cursor.execute(query)
+    #results = cursor.fetchall()
+    #st.write(results)
+with clm3:
+    st.header("TOP")
+
